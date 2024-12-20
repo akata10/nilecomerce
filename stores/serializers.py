e47ee8ba622c976ebd03a759b07b33751c4a3c96
unknown <akatadaniel@gmail.com>
2024-12-20 12:23:03 +0100

    veiws

--- stores/serializers.py
+++ stores/serializers.py
@@ -4,11 +4,36 @@ from . models import *
 
 # CATEGORY SERIALIZER
 
-class categorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Order
         fields='__all__'
+
 # PRODUCT SERIALIZER
+
+class productSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=product
+        fields='__all__'
+        
 # CART SERIALIZER
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Cart
+        fields='__all__'
+
+
 # ORDER SERIALIZER
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields='__all__'
+
 # CHECKOUT SERIALIZER
+
+class CheckoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        exclude=['cart','amount','order_status','subtotal','payment_complete','ref']
